33bb48f05c5799d553aebd45808c404be835587b
Karol <karolpl2000@onet.pl>
2023-02-06 19:53:35 +0100

    Files 06_02_2023

---
+++ build/CMakeFiles/Globus.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Globus.dir/main.cpp.obj"
+  "CMakeFiles/Globus.dir/main.cpp.obj.d"
+  "Globus.exe"
+  "Globus.exe.manifest"
+  "Globus.pdb"
+  "libGlobus.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Globus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
